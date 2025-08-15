0842a5ad07c6b7311f4659fca99077df2f39e11e
ShreyMadaan <89932358+ShreyMadaan@users.noreply.github.com>
2025-08-15 18:58:06 +0530

    feat: implement add, subtract, multiply, divide, modulus methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,31 @@
 public class Calculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero");
+            return 0;
+        }
+        return a / b;
+    }
+    public static double modulus(double a, double b) {
+        return a % b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Simple Calculator");
+        System.out.println("Add: " + add(5,3));
+        System.out.println("Subtract: " + subtract(5,3));
+        System.out.println("Multiply: " + multiply(5,3));
+        System.out.println("Divide: " + divide(5,3));
+        System.out.println("Modulus: " + modulus(5,3));
+
     }
 }
